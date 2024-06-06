92745ea23bd9b304806a0e44326de54d32b22119
Dima Denysenko <coding@dimdnk.com>
2024-06-06 10:34:29 +0300

    Initial version of Boutique generated by generator-jhipster@8.5.0

---
+++ src/main/webapp/app/shared/config/languages.ts
@@ -0,0 +1,9 @@
+const languages = () => ({
+  en: { name: 'English' },
+  ru: { name: 'Русский' },
+  fr: { name: 'Français' },
+  it: { name: 'Italiano' },
+  // jhipster-needle-i18n-language-key-pipe - JHipster will add/remove languages in this object
+});
+
+export default languages;
